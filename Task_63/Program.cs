de19a71b32335005eb8c2e72da7bea66b8e53386
Arcadiy <Motor-person@yandex.ru>
2022-10-31 23:11:11 +0400

    Сделали Task_63

--- Task_63/Program.cs
+++ Task_63/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.WriteLine("Пожалуйста, введите конечное число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+
+void NumbersRec(int arg1)
+{
+    if (arg1 == 0) return;
+    NumbersRec(arg1 - 1);
+    System.Console.Write($"{arg1} ");
+}
+
+NumbersRec(a);
--- Task_65/Program.cs
+++ Task_65/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear(); 
+
+Console.WriteLine("Пожалуйста, введите начальное число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Пожалуйста, введите конечное число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+
+string NumbersRec(int arg1, int arg2)
+{
+    if (arg1 <= arg2) return $"{arg1} " + NumbersRec(arg1 + 1, arg2);
+    else return String.Empty;
+}
+
+System.Console.WriteLine(NumbersRec(a, b));
+System.Console.WriteLine();
