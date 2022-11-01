9352224e8b096db58bfa4b6f137abfbd80d32cca
Arcadiy <Motor-person@yandex.ru>
2022-11-01 19:48:23 +0400

    Сделали Task_68

--- Task_68/Program.cs
+++ Task_68/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.WriteLine("Пожалуйста, введите число m");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Пожалуйста, введите число n");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int AckFunction(int arg1, int arg2)
+{
+    if (arg1 > 0 & arg2 == 0)
+        return AckFunction(arg1 - 1, 1);
+    if (arg1 > 0 & arg2 > 0)
+        return AckFunction(arg1 - 1, AckFunction(arg1, arg2 - 1));
+    return arg2 + 1;
+}
+
+Console.WriteLine($"Функция Аккермана для m = {m}, n = {n} равна {AckFunction(m, n)}");
