29f423b1d74f51797a97233ca6905bbbb38e746a
Arcadiy <Motor-person@yandex.ru>
2022-11-01 19:51:48 +0400

    Решили Task_68

--- Task_68/Program.cs
+++ Task_68/Program.cs
@@ -5,13 +5,13 @@ int m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Пожалуйста, введите число n");
 int n = Convert.ToInt32(Console.ReadLine());
 
-int AckFunction(int arg1, int arg2)
+int AckermannFunction(int arg1, int arg2)
 {
     if (arg1 > 0 & arg2 == 0)
-        return AckFunction(arg1 - 1, 1);
+        return AckermannFunction(arg1 - 1, 1);
     if (arg1 > 0 & arg2 > 0)
-        return AckFunction(arg1 - 1, AckFunction(arg1, arg2 - 1));
+        return AckermannFunction(arg1 - 1, AckermannFunction(arg1, arg2 - 1));
     return arg2 + 1;
 }
 
-Console.WriteLine($"Функция Аккермана для m = {m}, n = {n} равна {AckFunction(m, n)}");
+Console.WriteLine($"Функция Аккермана для m = {m}, n = {n} равна {AckermannFunction(m, n)}");
