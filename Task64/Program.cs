ebab31acb4c094b876e2ce78791d5176fe90463e
Arcadiy <Motor-person@yandex.ru>
2022-11-01 18:15:48 +0400

    Сделали Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.WriteLine("Пожалуйста, введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+void NumbersRec(int arg1)
+{
+    if (arg1 == 0) return;
+    System.Console.Write($"{arg1} ");
+    NumbersRec(arg1 - 1);
+}
+
+NumbersRec(a);
