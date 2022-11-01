5c8d3834913305508bc3bbf90f67c66085d9ab2b
Arcadiy <Motor-person@yandex.ru>
2022-11-01 15:43:47 +0400

    Сделали Task_69

--- Task_69/Program.cs
+++ Task_69/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.WriteLine("Пожалуйста, введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Пожалуйста, введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int PowerRec(int arg1, int arg2)
+{
+    return arg2 == 0 ? 1 : PowerRec(arg1, arg2 - 1) * arg1;
+    if (arg2 == 0) return 1;
+    else return PowerRec(arg1, arg2 - 1) * arg1;
+}
+System.Console.WriteLine(PowerRec(a, b));
